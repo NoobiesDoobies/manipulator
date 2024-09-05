8ca2f32818aab93fda53c8c8916b34dc3a6fb6e3
NoobiesDoobies <carlisoeryan20@gmail.com>
2024-09-05 15:31:28 +0700

    add RotateToFindObject action node

--- include/mobile_bot/pickup_box_bt.hpp
+++ include/mobile_bot/pickup_box_bt.hpp
@@ -4,7 +4,81 @@
 #include "behaviortree_cpp_v3/action_node.h"
 #include "behaviortree_cpp_v3/bt_factory.h"
 
-using namespace BT;
+#include "rclcpp/rclcpp.hpp"
+#include "std_msgs/msg/string.hpp"
+#include "std_msgs/msg/int32.hpp"
+#include "std_msgs/msg/float32.hpp"
+#include "std_msgs/msg/bool.hpp"
+#include "geometry_msgs/msg/twist_stamped.hpp"
+
+
+class RotateToFindObject: public BT::StatefulActionNode
+{
+  public:
+    RotateToFindObject(const std::string& name, const BT::NodeConfiguration& config):
+      BT::StatefulActionNode(name, config){
+        node_ = rclcpp::Node::make_shared(name);
+        twist_pub_ = node_->create_publisher<geometry_msgs::msg::TwistStamped>("/omnidirectional_controller/cmd_vel", 10);
+        is_detected_sub_ = node_->create_subscription<std_msgs::msg::Bool>("/aruco_detect/is_detected", 10, std::bind(&RotateToFindObject::isDetectedCallback, this, std::placeholders::_1));
+
+      }
+
+    static BT::PortsList providedPorts(){
+      return{BT::InputPort<float>("angular_speed")};
+    }
+
+    BT::NodeStatus onStart() override
+    {
+      getInput("angular_speed", angular_speed_);
+
+      RCLCPP_INFO(node_->get_logger(), "RotateToFindObject started with angular speed: %f", angular_speed_);
+      is_detected_ = false;
+      
+      return BT::NodeStatus::RUNNING;
+    }
+
+    BT::NodeStatus onRunning() override
+    {
+      if(is_detected_)
+      {
+        RCLCPP_INFO(node_->get_logger(), "Object detected");
+        return BT::NodeStatus::SUCCESS;
+      }
+      else
+      {
+        RCLCPP_INFO(node_->get_logger(), "Object not detected, Rotating to find the object");
+        auto twist_msg = std::make_shared<geometry_msgs::msg::TwistStamped>();
+        twist_msg->header.stamp = node_->now();
+        twist_msg->header.frame_id = "omnibot_base_link";
+        twist_msg->twist.angular.z = angular_speed_;
+
+        twist_pub_->publish(*twist_msg);
+        return BT::NodeStatus::RUNNING;
+      }
+
+
+    }
+
+    void onHalted() override
+    {
+      RCLCPP_INFO(node_->get_logger(), "RotateToFindObject halted");
+    }
+
+  private:
+    rclcpp::Node::SharedPtr node_;
+    rclcpp::Publisher<geometry_msgs::msg::TwistStamped>::SharedPtr twist_pub_;
+    rclcpp::Subscription<std_msgs::msg::Bool>::SharedPtr is_detected_sub_;
+    float angular_speed_;
+    bool is_detected_;
+
+    void isDetectedCallback(const std_msgs::msg::Bool::SharedPtr msg)
+    {
+      is_detected_ = msg->data;
+    }
+
+ 
+};
+
 
 // Example of custom SyncActionNode (synchronous action)
 // without ports.
@@ -16,10 +90,10 @@ public:
   {}
 
   // You must override the virtual function tick()
-  NodeStatus tick() override
+  BT::NodeStatus tick() override
   {
     std::cout << "ApproachObject: " << this->name() << std::endl;
-    return NodeStatus::SUCCESS;
+    return BT::NodeStatus::SUCCESS;
   }
 };
 
@@ -29,18 +103,18 @@ class GripperInterface
 public:
   GripperInterface() : _open(true) {}
 
-  NodeStatus open()
+  BT::NodeStatus open()
   {
     _open = true;
     std::cout << "Gripper open" << std::endl;
-    return NodeStatus::SUCCESS;
+    return BT::NodeStatus::SUCCESS;
   }
 
-  NodeStatus close()
+  BT::NodeStatus close()
   {
     _open = false;
     std::cout << "Gripper close" << std::endl;
-    return NodeStatus::SUCCESS;
+    return BT::NodeStatus::SUCCESS;
   }
 
 private:
--- src/pickup_box_bt.cpp
+++ src/pickup_box_bt.cpp
@@ -1,177 +1,28 @@
-#include "behaviortree_cpp_v3/action_node.h"
-#include "behaviortree_cpp_v3/bt_factory.h"
-#define DEFAULT_BT_XML "/home/carlios/manipulator_ws/src/manipulator/behaviour_trees/pickup_box.xml"
+#include "rclcpp/rclcpp.hpp"
+#include "mobile_bot/pickup_box_bt.hpp"
+#include <filesystem>
+#include <iostream>
+// #define DEFAULT_BT_XML "/home/carlios/manipulator_ws/src/manipulator/behaviour_trees/pickup_box.xml"
 
+int main(int argc, char **argv){
+    std::filesystem::path ros_ws_path = std::filesystem::current_path();
+    std::string xml_file_name = "/src/manipulator/behaviour_trees/pickup_box_bt3.xml";
+    std::string xml_path = ros_ws_path.string() + xml_file_name;
 
-using namespace BT;
-
-// Custom node classes declaration (or header inclusion)
-class ApproachObject : public BT::SyncActionNode
-{
-public:
-  ApproachObject(const std::string& name) :
-      BT::SyncActionNode(name, {})
-  {}
-
-  // You must override the virtual function tick()
-  NodeStatus tick() override
-  {
-    std::cout << "ApproachObject: " << this->name() << std::endl;
-    return NodeStatus::SUCCESS;
-  }
-};
-
-class IsArucoDetected : public BT::SyncActionNode
-{
-public:
-    IsArucoDetected(const std::string& name) : BT::SyncActionNode(name, {}) {}
-    BT::NodeStatus tick() override
-    {
-        // Custom logic to check for ArUco tag detection
-        // bool detected = detectArucoTag();
-        bool detected = false; // Dummy value
-        return detected ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
-    }
-};
-
-class IsCloseEnough : public BT::SyncActionNode
-{
-public:
-    IsCloseEnough(const std::string& name, const BT::NodeConfiguration& config)
-        : BT::SyncActionNode(name, config) {}
-    BT::NodeStatus tick() override
-    {
-        // Custom logic to check if the robot is close enough to the ArUco tag
-        // double distance = getDistanceToArucoTag();
-        double distance = 0.2; // Dummy value
-        return distance < 0.3 ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
-    }
-
-    static BT::PortsList providedPorts()
-    {
-        return { BT::InputPort<double>("distance") };
-    }
-};
-
-class RotateToSearch : public BT::AsyncActionNode
-{
-public:
-    RotateToSearch(const std::string& name) : BT::AsyncActionNode(name, {}), is_rotating(true) {}
-    
-    BT::NodeStatus tick() override
-    {
-        if (is_rotating)
-        {
-            // Custom logic to rotate and search for ArUco tags
-            // rotateRobot();
-            
-            // Simulate an ongoing rotation operation
-            return BT::NodeStatus::RUNNING;
-        }
-        else
-        {
-            return BT::NodeStatus::SUCCESS;
-        }
-    }
-
-    void halt() override
-    {
-        is_rotating = false; // Stop rotating when halted
-    }
-
-private:
-    bool is_rotating;
-};
-
-
-class LowerArm : public BT::SyncActionNode
-{
-public:
-    LowerArm(const std::string& name) : BT::SyncActionNode(name, {}) {}
-    BT::NodeStatus tick() override
-    {
-        // Custom logic to lower the arm
-        // lowerArm();
-        return BT::NodeStatus::SUCCESS;
-    }
-};
-
-class RaiseArm : public BT::SyncActionNode
-{
-public:
-    RaiseArm(const std::string& name) : BT::SyncActionNode(name, {}) {}
-    BT::NodeStatus tick() override
-    {
-        // Custom logic to raise the arm
-        // raiseArm();
-
-        return BT::NodeStatus::SUCCESS;
-    }
-};
-
-class OpenGripper : public BT::SyncActionNode
-{
-public:
-    OpenGripper(const std::string& name) : BT::SyncActionNode(name, {}) {}
-    BT::NodeStatus tick() override
-    {
-        // Custom logic to open the gripper
-        // gripper.open();
-        return BT::NodeStatus::SUCCESS;
-    }
-};
-
-class CloseGripper : public BT::SyncActionNode
-{
-public:
-    CloseGripper(const std::string& name) : BT::SyncActionNode(name, {}) {}
-    BT::NodeStatus tick() override
-    {
-        // Custom logic to close the gripper
-        // gripper.close();
-        return BT::NodeStatus::SUCCESS;
-    }
-};
-
-
-int main()
-{
-    // Create the factory
+    rclcpp::init(argc, argv);
     BT::BehaviorTreeFactory factory;
 
-    // Register custom nodes
-    factory.registerNodeType<ApproachObject>("ApproachObject");
-    factory.registerNodeType<IsArucoDetected>("IsArucoDetected");
-    // factory.registerNodeType<IsArucoDetected>("is_aruco_detected_after_rotating"); // Registering twice for different instances
-    factory.registerNodeType<IsCloseEnough>("IsCloseEnough");
-    factory.registerNodeType<RotateToSearch>("RotateToSearch");
-    factory.registerNodeType<LowerArm>("LowerArm");
-    factory.registerNodeType<RaiseArm>("RaiseArm");
-    factory.registerNodeType<OpenGripper>("OpenGripper");
-    factory.registerNodeType<CloseGripper>("CloseGripper");
+    BT::NodeBuilder rotate_to_find_object = [](const std::string& name, const BT::NodeConfiguration& config){
+        return std::make_unique<RotateToFindObject>(name, config);
+    };
 
-    // Register Gripper actions
+    factory.registerBuilder<RotateToFindObject>("RotateToFindObject", rotate_to_find_object);
 
-    // Create the tree from the XML file
-    auto tree = factory.createTreeFromFile(DEFAULT_BT_XML);
+    auto tree = factory.createTreeFromFile(xml_path);
 
-    // Loop to continuously tick the tree
-    while (true)
-    {
-        BT::NodeStatus status = tree.tickRoot();
-        if (status == BT::NodeStatus::SUCCESS)
-        {
-            std::cout << "Task completed successfully." << std::endl;
-            break;
-        }
-        else if (status == BT::NodeStatus::FAILURE)
-        {
-            std::cout << "Task failed." << std::endl;
-            break;
-        }
-
-        // Simulate a loop rate (sleep or delay) to control the tick frequency
-        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    BT::NodeStatus status = BT::NodeStatus::RUNNING;
+    while(rclcpp::ok() && status == BT::NodeStatus::RUNNING){
+        status = tree.tickRoot();
     }
 
     return 0;
